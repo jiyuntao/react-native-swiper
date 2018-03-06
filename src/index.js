1e3255b63a4e66ae174ecdaf078b92e12b8a8a37
Jiyuntao <jiyuntao@tftiancai.com>
2018-03-06 18:52:07 +0800

    react-native-swiper: 增加initState.offset未定义
    Signed-off-by: Jiyuntao <jiyuntao@tftiancai.com>

--- src/index.js
+++ src/index.js
@@ -258,6 +258,7 @@ export default class extends Component {
 
     this.internals = {
       ...this.internals,
+      offset: initState.offset,
       isScrolling: false
     };
     return initState
